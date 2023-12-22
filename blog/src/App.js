bf623e5d05fbf1a04cf29d9c0c9c40851efc2286
진성민 <sengmin14@naver.com>
2023-12-22 15:38:16 +0900

    refactor : 반복되는 UI map함수로 수정 - 리스트 출력 - 좋아요 표시 - 상세보기 Modal

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,7 +5,8 @@ import { useState } from 'react';
 function App() {
     let post = '강남 우동 맛집';
     let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
-    let [따봉, 따봉변경] = useState(0);
+    let [따봉, 따봉변경] = useState([0, 0, 0]);
+    let [modal, setModal] = useState(false);
 
     return (
         <div className="App">
@@ -13,7 +14,7 @@ function App() {
                 <h4>ReactBlog</h4>
             </div>
 
-            <button
+            {/* <button
                 onClick={() => {
                     let copy = [...글제목];
                     copy.sort();
@@ -31,31 +32,60 @@ function App() {
                 }}
             >
                 글수정
-            </button>
-            <div className="list">
-                <h4>
-                    {글제목[0]}
-                    <span
-                        onClick={() => {
+            </button> */}
+            {/* <div className="list">
+                <h4>{글제목[0]}
+                    <span onClick={() => {
                             따봉변경(따봉 + 1);
-                        }}
-                    >
-                        👍
-                    </span>{' '}
-                    {따봉}
+                        }}>👍
+                    </span>{따봉}
                 </h4>
                 <p>2월 17일 발행</p>
             </div>
+
+            
+
             <div className="list">
-                <h4>{글제목[1]}</h4>
+                <h4 onClick={() => {
+                        modal === true ? setModal(false) : setModal(true);
+                    }}>{글제목[2]}
+                </h4>
                 <p>2월 17일 발행</p>
-            </div>
-            <div className="list">
-                <h4>{글제목[2]}</h4>
-                <p>2월 17일 발행</p>
-            </div>
+            </div> */}
+
+            {
+                글제목.map((a, i) => {
+                    return (
+                    <div className="list" key={i}>
+                        <h4 onClick={() => {
+                        modal === true ? setModal(false) : setModal(true);
+                        }}>{글제목[i]}
+                        <span onClick={() => {
+                            let copy = [...따봉];
+                            copy[i]++;
+                            따봉변경(copy);
+                        }}>👍
+                        </span>{따봉[i]}
+                        </h4> 
+                        <p>2월 17일 발행</p>
+                    </div>
+                    )   
+                })
+            }
+
+            {modal == true ? <Modal /> : null}
         </div>
     );
 }
+
+const Modal = () => {
+    return (
+        <div className="modal">
+            <h4>글제목</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+    );
+};
 
 export default App;
