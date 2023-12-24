31080b04ce198a2ba51404d94fcd19e895594075
진성민 <sengmin14@naver.com>
2023-12-24 16:49:59 +0900

    feat : 리스트 추가 및 삭제 기능 추가 - 추가 및 삭제 기능 - 추가 시 날짜 기능 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,6 +7,8 @@ function App() {
     let [따봉, 따봉변경] = useState([0, 0, 0]);
     let [modal, setModal] = useState(false);
     let [title, setTitle] = useState(0);
+    let [입력값, 입력값변경] = useState('');
+    let [날짜, 날짜변경] = useState(['2023년 12월 1일', '2023년 12월 1일', '2023년 12월 1일'])
 
     return (
         <div className="App">
@@ -20,18 +22,56 @@ function App() {
                         <h4 onClick={() => {
                         modal === true ? setModal(false) : setModal(true);
                         setTitle(i)}}>{글제목[i]}
-                            <span onClick={() => {
+                            <span onClick={(e) => { e.stopPropagation()
                                 let copy = [...따봉];
                                 copy[i]++;
                                 따봉변경(copy);
                             }}>👍
                             </span>{따봉[i]}
                         </h4>
-                        <p>2월 17일 발행</p>
+                        <p>{날짜[i]}</p>
+                        <button onClick={(e) => {
+                                e.stopPropagation()
+                                let copy = [...글제목];
+                                copy.splice(i, 1);
+                                글제목변경(copy);
+
+                                let goodCopy = [...따봉];
+                                goodCopy.splice(i, 1);
+                                따봉변경(goodCopy);
+
+                                let dateCopy = [...날짜];
+                                dateCopy.splice(i, 1);
+                                날짜변경(dateCopy);
+                        }}>삭제</button>
                     </div>
                     )   
                 })
             }
+            
+
+            <input onChange={(e) => {
+                입력값변경(e.target.value)
+                // console.log(입력값)
+            }}/>
+            <button onClick={() => {
+                if(입력값.trim() !== '') {
+                    let copy = [...글제목]
+                    copy.unshift(입력값)
+                    글제목변경(copy);
+
+                    let goodCopy = [...따봉]
+                    goodCopy.unshift(0)
+                    따봉변경(goodCopy);
+
+                    let today = new Date();
+                    let formattedDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`
+                    let dateCopy = [...날짜];
+                    dateCopy.unshift(formattedDate);
+                    날짜변경(dateCopy);
+                }
+                
+            }}>글발행</button>
             
 
             {
