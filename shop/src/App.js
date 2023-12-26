012c90b48db28c16e12a3433f8ff5aa18cb5a2de
진성민 <sengmin14@naver.com>
2023-12-26 21:03:50 +0900

    feat : 기본 UI 제작

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,30 +1,44 @@
-import { useState } from 'react';
+import {Navbar,Container, Nav, Row, Col} from 'react-bootstrap';
 import './App.css';
 
+
 function App() {
-  const[person, changePerson] = useState([{
-    name : '진성민',
-    age : 21,
-  }])
-
-  const[personName, changePersonName] = useState('');
-  const[personAge, changePersonAge] = useState('');
-
+  
   return (
     <div className="App">
-      <div>{person[0].name}</div>
-      <div>
-      <input type='text' placeholder='name' onChange={(e) => {
-        changePersonName(e.target.value);
-      }}></input>
-      <input type='text' placeholder='age' onChange={(e) => {
-        changePersonAge(e.target.value);
-      }}></input>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">shoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-      <button onClick={() => {
-        let copy = {...person};
-      }}>버튼</button>
-      </div>
+
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
+      
+
     </div>
   );
 }
