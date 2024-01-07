3379bbddd7c831598982c977c6b2c793131733b8
진성민 <sengmin14@naver.com>
2024-01-07 13:10:11 +0900

    feat : 라우트 기능 추가 - 상세페이지 0, 1, 2 추가 - 메인화면 꾸미기 - 제품 리스트 map함수를 통해 출력 - 제품 데이터 data.js에 저장

--- shop/src/App.js
+++ shop/src/App.js
@@ -18,10 +18,11 @@ function App() {
           <Navbar.Brand href="/">shoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={() => {navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={() => {navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/detail/0')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
 
       <Routes>
         <Route path="/" element={
@@ -35,7 +36,7 @@ function App() {
             {
               shoes.map((a, i) => {
               return (
-                <Card shoes={shoes[i]} i={i+1}/>
+                  <Card shoes={shoes[i]} i={i+1}/>
               )
               })
             }
@@ -46,7 +47,6 @@ function App() {
 
         <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
 
-        
         <Route path="/about" element={<About/>}>
           <Route path="member" element={<div>멤버임</div>}/>
           <Route path="location" element={<div>위치정보임</div>}/>
